f591bda931388ad62a60ab12028f6fa114f04962
breakbeatero <juannunezsierra@gmail.com>
2014-06-28 20:58:26 +0200

    asdasd

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-	res.send('holaaaaaaaa' + process.env.PORT);
+	res.send('Servidor corriendo en NodeJS en el puerto ' + process.env.PORT);
 });
 
 var port = Number(process.env.PORT || 5000);
