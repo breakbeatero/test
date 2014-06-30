d39a522396ec3cd7661ead6128ca791bbf5baebb
breakbeatero <juannunezsierra@gmail.com>
2014-06-30 21:47:08 +0200

    mongo

--- web.js
+++ web.js
@@ -3,6 +3,13 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+
+
+
+
+
+
+
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
@@ -12,4 +19,16 @@ app.get('/', function(req, res) {
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+
+var uri = mongodb://breakbeatero:djjuanmy1@ds049219.mongolab.com:49219/breakbeatero
+
+
+
+var mongodb = require('mongodb');
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb'; 
+
+mongodb.MongoClient.connect(uri, function (err, db) {
+	res.send('conectado');
 });
